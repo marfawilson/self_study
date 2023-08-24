83674787f7de5e97dc730f269ba860a21eab27de
Marissa Wilson <marfawilsoncode@gmail.com>
2023-08-24 12:09:17 -0500

    Push of execution lecture + notes

--- currently_studying/JSWeirdParts_MarissaWilson/B6_Execution/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/B6_Execution/app.js
@@ -1,9 +1,12 @@
-b();
-
-console.log(a);
-
-var a = 'Hello World!';
-
+// the function below is stored in memory during creation
 function b() {
     console.log('Called b!');
 }
+
+b();
+
+console.log(a); // during creation, variable a is in memory, but has no value (therefore 'undefined')
+
+var a = 'Hola Mundo!';
+
+console.log(a); // during execution, code is run line by line, so here we get back 'Hola Mundo!' because now it has been assigned a value
