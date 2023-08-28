6f7658636b924adc9257c1825e5b37edaf5fce6c
Marissa Wilson <marfawilsoncode@gmail.com>
2023-08-28 12:13:24 -0500

    Push of scope chain lecture + notes

--- currently_studying/JSWeirdParts_MarissaWilson/B10_ScopeChain/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/B10_ScopeChain/app.js
@@ -3,9 +3,14 @@ function b() {
 }
 
 function a() {
+	// now that function b is in a new lexical environment, myVar's value is 2
+	// function b() {
+	// 	console.log(myVar);
+	// }
+
 	var myVar = 2;
 	b();
 }
 
 var myVar = 1;
-a();
+a();// returns a value of 1; function b is in the same lexical environment (level) as the GEC
