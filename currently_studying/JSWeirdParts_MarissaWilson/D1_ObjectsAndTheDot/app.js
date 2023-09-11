c7123d647edc0ae18073af407c37e85926873831
Marissa Wilson <marfawilsoncode@gmail.com>
2023-09-11 11:38:43 -0500

    Additional notes

--- currently_studying/JSWeirdParts_MarissaWilson/D1_ObjectsAndTheDot/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/D1_ObjectsAndTheDot/app.js
@@ -9,12 +9,13 @@ var firstNameProperty = "firstname";
 console.log(person); // returns the entire person object
 console.log(person[firstNameProperty]);// returns first name via computed member access
 
-// the code below makes use of the member access operator (easier to type & read; higher precedence than computed member access)
+// the code below makes use of the member access operator (easier to type & read; higher precedence than computed member access). they are both primitive properties.
 console.log(person.firstname);
 console.log(person.lastname);
 
 person.address = new Object(); // creation of a new object property within the person object
 
+// adding properties to the sub-object address within the person object. the member access operator has left-to-right associativity.
 person.address.street = "123 Maple Ln";
 person.address.city = "Austin";
 person.address.state = "TX";
