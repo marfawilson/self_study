a201bb12d168b1dc263ad33bc539fb171ae26d45
Marissa Wilson <marfawilsoncode@gmail.com>
2023-09-12 12:47:52 -0500

    Push of faking namespaces lecture + notes

--- currently_studying/JSWeirdParts_MarissaWilson/D3_FakingNamespaces/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/D3_FakingNamespaces/app.js
@@ -1,4 +1,18 @@
+// namespace: a container for variables & functions, typically to keep variables & functions with the same name separate.
+
 var greet = 'Hello!';
 var greet = 'Hola!'; 
 
-console.log(greet);
+console.log(greet); // returns 'Hola!' because JS synchronously runs each line in order
+
+// these objects will serve as containers to help us 'fake' a namespace in JS
+var english = {
+    greetings: {
+        basic: 'Hello!'
+    }
+};
+var spanish = {};
+
+spanish.greet = 'Hola!';
+
+console.log(english); // returns 'Hello!'
