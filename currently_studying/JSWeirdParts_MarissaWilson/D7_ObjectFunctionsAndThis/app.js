5abf585b3baf13a937afd74790f1ae9ed5f1bb28
Marissa Wilson <marfawilsoncode@gmail.com>
2023-09-20 12:47:39 -0500

    Added notes to objects functions and this lecture

--- currently_studying/JSWeirdParts_MarissaWilson/D7_ObjectFunctionsAndThis/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/D7_ObjectFunctionsAndThis/app.js
@@ -1,32 +1,38 @@
+// when functions or function expressions are on the global level, 'this' will point to the window object
 function a() {
     console.log(this);
-    this.newVariable = 'hello';
+    this.newVariable = 'hello'; // attaches to the global object, not the function
 }
 
 var b = function() {
     console.log(this);
 }
 
-a();
+/* a new execution context is created every time a function is invoked, however 'this' still points to the window object despite having more than one execution context */
+a(); 
 
-console.log(newVariable);
+console.log(newVariable); // returns 'hello'
 
 b();
+
+/* when a function is actually a method attached to an object, 'this' points to the object that method is sitting inside of instead of the global window object */
 
 var c = {
     name: 'The c object',
     log: function() {
-        var self = this;
+        /* on line 25, 'this' points to the c object, so we store it into a variable to avoid accidentally mutating the global object */
 
-        self.name = 'Updated c object';
-        console.log(self);
+        var self = this; 
+
+        self.name = 'Updated c object'; // mutates name property of c object
+        console.log(self); // points to the c object
 
         var setName = function(newName) {
-            self.name = newName;
+            self.name = newName; 
         }
         setName('Updated again! The c object');
         console.log(self);
     }
 }
 
-c.log();
+c.log(); // returns c object twice with both mutated names vs repeating the first mutation & changing the global object
