b5c3c3943e57cf23ee85513b5f8b9e5728bfb252
Marissa Wilson <marfawilsoncode@gmail.com>
2023-09-22 10:16:16 -0500

    Added notes to arguments lecture

--- currently_studying/JSWeirdParts_MarissaWilson/D8_Arguments/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/D8_Arguments/app.js
@@ -1,6 +1,11 @@
+/* arguments: the parameters you pass to a function.
+    JS gives you a keyword of the same name which contains them all */
+
 function greet(firstName, lastName, language) {
 
-    language = language || 'en';
+    language = language || 'en'; // here we are using the default value concept to set a default parameter so it doesn't just return 'undefined' 
+
+// the if statement on lines 10-14 checks for the existence of parameters
 
     if (arguments.length === 0) {
         console.log('Missing parameters!');
@@ -11,13 +16,15 @@ function greet(firstName, lastName, language) {
     console.log(firstName);
     console.log(lastName);
     console.log(language);
-    console.log(arguments);
-    console.log('arg 0: ' + arguments[0]);
+    console.log(arguments); // contains an array-like list of all the values we've passed as parameters
+    console.log('arg 0: ' + arguments[0]); // here we are using the brackets operator to access the value of an argument
     console.log('----------');
 
 }
 
-greet();
-greet('Thurman');
+greet(); // JS won't throw an error if you don't include any arguments, even if they are expected. due to hoisting, this returns ''undefined' for each argument.
+
+greet('Thurman'); // arguments are processed left to right, so this is considered the argument for 'firstName'
+
 greet('Thurman', 'Murman');
 greet('Thurman', 'Murman', 'es');
