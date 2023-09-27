cbbe033cae06e609432485d6705f098aaede80c5
Marissa Wilson <marfawilsoncode@gmail.com>
2023-09-27 18:36:56 -0500

    Push of whitespace lecture + notes

---
+++ currently_studying/JSWeirdParts_MarissaWilson/D12_Whitespace/app.js
@@ -0,0 +1,28 @@
+/* white space: invisible characters that create literal 'space' in your written code.
+this includes carriage returns, tabs, spaces, and comments such as this multiline comment, or the examples below.  */
+
+var 
+    //first name of the person
+    firstname, 
+    
+    // last name of the person
+    lastname, 
+    
+    // the language
+    // can be 'en' or 'es'
+    language;
+
+var person = {
+
+    // the first name
+    firstname: 'Yuji',
+
+    // the last name
+    // (always required)
+    lastname: 'Itadori'
+}
+
+console.log(person);
+
+// comments can be helpful for you if it's been a long time since you've looked at this particular code.
+// they can also be helpful to other people working on or with your code to help them quickly understand what's going on.
