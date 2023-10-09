594d722f7e893a78ba4a0063076b65f67820f2a2
Marissa Wilson <marfawilsoncode@gmail.com>
2023-10-09 16:13:04 -0500

    Added notes to function factories lecture

--- currently_studying/JSWeirdParts_MarissaWilson/D17_FunctionFactories/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/D17_FunctionFactories/app.js
@@ -1,4 +1,7 @@
+// function factory: a function that 'makes' or returns other things for me
 function makeGreeting(language){
+
+// this inner function has access to the argument passed through it when it was created (invoked). each invocation is a new execution context.
 
     return function(firstName, lastName) {
 
@@ -12,6 +15,8 @@ function makeGreeting(language){
 
 }
 
+/* each time a function is invoked, a new execution context is created. 
+even though the invocations happening on lines 20 and 21 are using the same function, they point to different spots in memory, each with different parameters for language. */
 var greetEnglish = makeGreeting('en');
 var greetSpanish = makeGreeting('es');
 
