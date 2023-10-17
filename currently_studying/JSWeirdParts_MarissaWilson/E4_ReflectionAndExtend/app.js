846ae90b5221c0f6b07d6c18e861c8bdfdfe4e88
Marissa Wilson <marfawilsoncode@gmail.com>
2023-10-17 13:31:24 -0500

    Push of reflection and extend lecture + notes

--- currently_studying/JSWeirdParts_MarissaWilson/E4_ReflectionAndExtend/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/E4_ReflectionAndExtend/app.js
@@ -1,3 +1,5 @@
+// reflection: an object can look at itself, listing & changing its properties & methods
+
 var person = {
     firstname: 'Default',
     lastname: 'Default',
@@ -14,9 +16,26 @@ var sakura = {
 // don't do this EVER! for demo purposes only!!!
 sakura.__proto__ = person;
 
+// reflection of metadata (looking at its own properties)
+for (var prop in sakura) {
+    if (sakura.hasOwnProperty(prop)) {
+        console.log(prop + ': ' + sakura[prop]);
+    }
+}
 
 
+var kero = {
+    address: 'Osaka, Japan',
+    getFormalFullName: function() {
+        return this.lastname + ' ' + this.firstname;
+    }
+}
 
+var tomoyo = {
+    getFirstName: function() {
+        return firstname;
+    }
+}
 
-
-
+_.extend(sakura, kero, tomoyo); // this method composes an object by adding the properties of other objects passed in as parameters
+console.log(sakura);
