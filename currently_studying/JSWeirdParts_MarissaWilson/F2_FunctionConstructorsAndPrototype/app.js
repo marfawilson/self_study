4771af519f431037b043658c7eb20bdb05f293f5
Marissa Wilson <marfawilsoncode@gmail.com>
2023-10-19 11:53:56 -0500

    Push of function constructors and prototype lecture + notes

--- currently_studying/JSWeirdParts_MarissaWilson/F2_FunctionConstructorsAndPrototype/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/F2_FunctionConstructorsAndPrototype/app.js
@@ -7,8 +7,20 @@ function Person(firstname, lastname) {
     
 }
 
+// putting methods on the prototype vs in the function constructor saves memory, because they only need one 'copy' to be used
+Person.prototype.getFullName = function() {
+    return this.firstname + ' ' + this.lastname;
+}
+
 var usagi = new Person('Usagi', 'Tsukino');
 console.log(usagi);
 
 var ami = new Person('Ami', 'Mizuno');
 console.log(ami);
+
+// adding properties & methods to the prototype gives other objects access to those items 
+Person.prototype.getFormalFullName = function() {
+    return this.lastname + ', ' + this.firstname;
+}
+
+console.log(usagi.getFormalFullName());
