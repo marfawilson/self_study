d28a97510eb43072d60ffb4b6f7527dfae41dfd3
Marissa Wilson <marfawilsoncode@gmail.com>
2023-11-02 16:10:00 -0500

    Included additional notes

--- currently_studying/JSWeirdParts_MarissaWilson/I2_DeepDiveJQuery/app.js
+++ currently_studying/JSWeirdParts_MarissaWilson/I2_DeepDiveJQuery/app.js
@@ -1,2 +1,5 @@
-var q = $("ul.people li");
+// method chaining: calling one method after another, and each method affects the parent object
+// so obj.method1().method2(), where both methods end up with a 'this' variable pointing at 'obj'
+
+var q = $("ul.people").addClass("newClass").removeClass("people");
 console.log(q);
