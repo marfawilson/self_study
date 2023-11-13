9159cea3e9c8ca581117e69644259fe2697a222b
Marissa Wilson <marfawilsoncode@gmail.com>
2023-11-13 15:16:17 -0600

    Added notes

--- currently_studying/JSWeirdParts_MarissaWilson/J3_OurObjectAndPrototype/Greetr.js
+++ currently_studying/JSWeirdParts_MarissaWilson/J3_OurObjectAndPrototype/Greetr.js
@@ -6,19 +6,22 @@
         return new Greetr.init(firstName, lastName, language);
     }
 
-    Greetr.prototype = {};
+    Greetr.prototype = {}; // empty object (prototype) for us to store methods onto the Greetr object
 
+    // initializing Greetr.init after calling it in the function on line 5 is ok because everything will already be in memory
     Greetr.init = function(firstName, lastName, language) {
 
-        var self = this; // storing 'this' into a variable to avoid confusion later 
+        var self = this; // storing 'this' into a variable to avoid confusion later
+        
+        // setting default parameters
         self.firstName = firstName || '';
         self.lastName = lastName || '';
         self.language = language || 'en';
 
     }
 
-    Greetr.init.prototype = Greetr.prototype;
+    Greetr.init.prototype = Greetr.prototype; // objects created with Greetr.init point to Greetr.prototype
 
-    global.Greetr = global.G$ = Greetr;
+    global.Greetr = global.G$ = Greetr; // attaching the Greetr function to the global object with an alias
 
 }(window, jQuery));
