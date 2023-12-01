31f8e3139003dd92557144408b498a83b7691b9b
Marissa Wilson <marfawilsoncode@gmail.com>
2023-12-01 11:44:08 -0600

    Created initial files

---
+++ currently_studying/JS_ES6AndBeyond_MarissaWilson/B2_LetAndConst_Let/app.js
@@ -0,0 +1,45 @@
+// example 1
+
+
+
+// example 2
+
+
+
+// example 3
+
+
+
+// example 4
+
+
+
+// example 5
+
+
+
+// example 6
+
+
+
+// example 7
+
+
+
+// example 8
+
+
+
+// example 9
+
+
+
+// example 10
+
+
+
+// example 11
+
+
+
+// example 12
