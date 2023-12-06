5131c769b27d8e9c60570d4e88fe114ea0bc5d69
Marissa Wilson <marfawilsoncode@gmail.com>
2023-12-06 13:20:07 -0600

    Push of multiline strings lecture w/  examples + notes

---
+++ currently_studying/JS_ES6AndBeyond_MarissaWilson/C2_Templates_MultilineStrings/examples/example1.js
@@ -0,0 +1,9 @@
+const firstname = 'Marissa';
+const lastname = 'Wilson';
+
+// two ways to indicate white space, in this case, a new line
+const fullname = 'Marissa\nWilson';
+const fullname2 = `Marissa
+Wilson`;
+
+console.log(fullname === fullname2); // returns true
---
+++ currently_studying/JS_ES6AndBeyond_MarissaWilson/C2_Templates_MultilineStrings/examples/example2.js
@@ -0,0 +1,9 @@
+const firstname = 'Marissa';
+const lastname = 'Wilson';
+
+// use backticks in order to use template literals & create multiline strings easier
+const fullname = `Hello,
+` + firstname + `
+` + lastname;
+
+console.log(fullname);
