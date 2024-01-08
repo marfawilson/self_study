a8b7ce8d2e8ac10a0b02ad70f8d13052ee216040
Marissa Wilson <marfawilsoncode@gmail.com>
2024-01-08 16:43:39 -0600

    Completed extends lecture, added notes

--- currently_studying/JS_ES6AndBeyond_MarissaWilson/D7_Classes_Extends/examples/example1.js
+++ currently_studying/JS_ES6AndBeyond_MarissaWilson/D7_Classes_Extends/examples/example1.js
@@ -10,6 +10,7 @@ class Person {
     }
 }
 
+// the 'extends' keyword sets up the prototype chain
 class Marissa extends Person {
     meet() {
         return `Nice to meet you`;
@@ -22,6 +23,22 @@ console.log(me.meet());
 console.log(me.__proto__);
 console.log(me.__proto__ === Marissa.prototype); // true
 console.log(me.__proto__.__proto__);
+console.log(Marissa.prototype.__proto__ === Person.prototype); // true
+console.log(Marissa.__proto__); // returns the original person class object (connected by 'extends')
 
 let me2 = new Person('Araceli');
-console.log(me2.greet());
+
+/*
+console.log(me2.meet()); // throws an error because the meet method is on 'Marissa', not 'Person'
+*/
+
+// function constructors & built in types can be extended (if they are constructable)
+class myNumber extends Number {
+    addOne(){
+        return this + 1;
+    }
+}
+
+let num = new myNumber(525600);
+console.log(num.toFixed(1));
+console.log(num.addOne());
