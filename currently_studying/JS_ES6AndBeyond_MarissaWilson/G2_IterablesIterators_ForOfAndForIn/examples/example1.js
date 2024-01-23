8be967d54806589ac1cc3b63e0c7ee83cc51b470
Marissa Wilson <marfawilsoncode@gmail.com>
2024-01-23 11:08:51 -0600

    Push of for of/for in lecture w/example + notes

--- currently_studying/JS_ES6AndBeyond_MarissaWilson/G2_IterablesIterators_ForOfAndForIn/examples/example1.js
+++ currently_studying/JS_ES6AndBeyond_MarissaWilson/G2_IterablesIterators_ForOfAndForIn/examples/example1.js
@@ -7,5 +7,48 @@ for (const name of names) {
 
 // if you want to know how the object is structured
 for (const name in names) {
-    console.log(name); // keys
+    console.log(name); // keys (property names)
+}
+
+//objects
+class Animal {
+    name = 'dog';
+}
+
+class Dog extends Animal {
+    sound = 'bark';
+
+    // methods are not enumerable in class syntax
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+let ein = new Dog();
+
+for (const prop in ein) {
+    console.log(prop);
+}
+
+// properties in object literal syntax are enumerable by default
+let ein2 = {
+    sound: 'bark',
+    makeSound: function() {
+        console.log('bark');
+    }
+}
+
+ein2.__proto__ = {
+    residence: 'Earth'
+}
+
+for (const prop in ein2) {
+    console.log(prop);
+}
+
+// how to check if a specific property is enumarable
+console.log(ein.propertyIsEnumerable('sound'));
+
+for (const prop of ein) {
+    console.log(prop);  // TypeError: ein is not iterable
 }
