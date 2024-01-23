a206ed4214d242369e667f470b0f171d4fd1dc7d
Marissa Wilson <marfawilsoncode@gmail.com>
2024-01-23 11:59:52 -0600

    Push of iterators lecture w/example + notes

---
+++ currently_studying/JS_ES6AndBeyond_MarissaWilson/G3_IterablesIterators_Iterators/examples/example1.js
@@ -0,0 +1,29 @@
+let numbers = {
+    // the iterator that is part of the numbers object
+    getIterator: function(start, end) {
+        let nextValue = start;
+        return {
+            next: () => {
+                if (nextValue < end) {
+                    let nextObj = { value: nextValue, done: false };
+                    nextValue++;
+                    return nextObj;
+                }
+                return { value: nextValue, done: true };
+            }
+        }
+    }
+}
+
+
+let iterator = numbers.getIterator(2, 5);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+let iterator2 = numbers.getIterator(8, 10);
+console.log(iterator2.next());
+console.log(iterator2.next());
+console.log(iterator2.next());
