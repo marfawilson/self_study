d31fe82aa46f4f003a30fe772489515fc2e7a75d
Marissa Wilson <marfawilsoncode@gmail.com>
2024-02-13 14:10:39 -0600

    Created populateUI function to update document

--- currently_studying/VanillaJS_WebProjects_MW/movie_seat_booking/script.js
+++ currently_studying/VanillaJS_WebProjects_MW/movie_seat_booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -23,7 +25,26 @@ function updateSelectedCount() {
 	const selectedSeatsCount = selectedSeats.length;
 
 	count.innerText = selectedSeatsCount;
-	total.innerText = selectedSeatsCount * ticketPrice;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Get data from local storage & populate UI
+function populateUI() {
+	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+	if (selectedSeats !== null && selectedSeats.length > 0) {
+		seats.forEach((seat, index) => {
+			if (selectedSeats.indexOf(index) > -1) {
+			seat.classList.add('selected');
+			}
+		});
+	}
+
+	const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+	if(selectedMovieIndex !== null) {
+		movieSelect.selectedIndex = selectedMovieIndex;
+	}
 }
 
 // Movie select event
@@ -36,11 +57,14 @@ movieSelect.addEventListener('change', (e) => {
 // Seat click event
 container.addEventListener('click', (e) => {
 	if (
-		e.target.classList.contains('seat') &&
-		!e.target.classList.contains('occupied')
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
 	) {
-		e.target.classList.toggle('selected');
+    e.target.classList.toggle('selected');
 
-		updateSelectedCount();
+    updateSelectedCount();
 	}
 });
+
+// Initial count & total set
+updateSelectedCount();
