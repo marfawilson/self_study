fbae1387c95049dafc3ce6a481a139a7cac047c3
Marissa Wilson <marfawilsoncode@gmail.com>
2024-04-04 14:57:42 -0500

    Refactored App.jsx file by storing Header component in it's own file + components folder

--- currently_studying/React_CompleteGuide_MW/01-starting-project/src/App.jsx
+++ currently_studying/React_CompleteGuide_MW/01-starting-project/src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src = {reactImg} alt = "Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src = {props.image} alt = {props.description} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -41,12 +11,13 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           <CoreConcept 
-          title = 'Components' 
-          description = 'The core UI building block'
-          image = {componentsImg} />
-          <CoreConcept />
-          <CoreConcept />
-          <CoreConcept />
+          title = {CORE_CONCEPTS[0].title} 
+          description = {CORE_CONCEPTS[0].description}
+          image = {CORE_CONCEPTS[0].image} />
+
+          <CoreConcept {...CORE_CONCEPTS[1]} />
+          <CoreConcept {...CORE_CONCEPTS[2]} />
+          <CoreConcept {...CORE_CONCEPTS[3]} />
         </ul>
         </section>
         
