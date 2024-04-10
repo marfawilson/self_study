03ed40cf75cfe83e087fee92410084ffe7ac485c
Marissa Wilson <marfawilsoncode@gmail.com>
2024-04-10 12:34:20 -0500

    Refactored TabButton component with handleSelect event listener function

--- currently_studying/React_CompleteGuide_MW/01-starting-project/src/App.jsx
+++ currently_studying/React_CompleteGuide_MW/01-starting-project/src/App.jsx
@@ -5,6 +5,12 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
+}
+
   return (
     <div>
       <Header />
@@ -26,11 +32,12 @@ function App() {
         <section id = 'examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect = {() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect = {() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect = {() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect = {() => handleSelect('state')}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
         
       </main>
