bfe9d872effc51e408811a4caad4c877e07d8367
Marissa Wilson <marfawilsoncode@gmail.com>
2024-04-26 13:12:48 -0500

    Splitting CoreConcepts component feature into seperate file

--- currently_studying/React_CompleteGuide_MW/01-starting-project/src/App.jsx
+++ currently_studying/React_CompleteGuide_MW/01-starting-project/src/App.jsx
@@ -19,29 +19,22 @@ function App() {
     <Fragment>
       <Header />
       <main>
-        <section id = 'core-concepts'>
-        <h2>Core Concepts</h2>
-        <ul>
-          {CORE_CONCEPTS.map((item) => <CoreConcept key= {item.title} {...item} />)}
-        </ul>
-        </section>
-
         <section id = 'examples'>
           <h2>Examples</h2>
-          <menu>
-            <TabButton 
-            isSelected={selectedTopic === 'components'} 
-            onSelect = {() => handleSelect('components')}>Components</TabButton>
-            <TabButton
-            isSelected={selectedTopic === 'jsx'}
-            onSelect = {() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton
-            isSelected={selectedTopic === 'props'}
-            onSelect = {() => handleSelect('props')}>Props</TabButton>
-            <TabButton 
-            isSelected={selectedTopic === 'state'}
-            onSelect = {() => handleSelect('state')}>State</TabButton>
-          </menu>
+            <menu>
+              <TabButton 
+              isSelected={selectedTopic === 'components'} 
+              onSelect = {() => handleSelect('components')}>Components</TabButton>
+              <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect = {() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect = {() => handleSelect('props')}>Props</TabButton>
+              <TabButton 
+              isSelected={selectedTopic === 'state'}
+              onSelect = {() => handleSelect('state')}>State</TabButton>
+            </menu>
 
           {!selectedTopic ? <p>Please select a topic.</p> : 
             (<div id = 'tab-content'>
