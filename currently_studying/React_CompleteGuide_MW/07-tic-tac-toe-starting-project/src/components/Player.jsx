1382ff9ec6124ce9a16ca13eed6a728d2328fcce
Marissa Wilson <marfawilsoncode@gmail.com>
2024-05-11 15:20:11 -0500

    Updated state of player name input via two-way binding

---
+++ currently_studying/React_CompleteGuide_MW/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit() {
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleNameChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let newPlayerName = <span className='player-name'>{playerName}</span>;
+    // let btnCaption = 'Edit';
+
+    if(isEditing) {
+        newPlayerName = <input type = 'text' required value = {playerName} onChange = {handleNameChange} />;
+        // btnCaption = 'Save';
+    }
+
+    return (
+        <li>
+            <span className='player'>
+                {newPlayerName}
+                <span className='player-symbol'>{symbol}</span>
+            </span>
+            <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
+}
