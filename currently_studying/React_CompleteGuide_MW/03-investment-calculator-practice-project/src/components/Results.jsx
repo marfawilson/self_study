fbeb48d6dc823568a9b9733de7f8731e7ea0980b
Marissa Wilson <marfawilsoncode@gmail.com>
2024-05-22 16:02:56 -0500

    Created initial results table

--- currently_studying/React_CompleteGuide_MW/03-investment-calculator-practice-project/src/components/Results.jsx
+++ currently_studying/React_CompleteGuide_MW/03-investment-calculator-practice-project/src/components/Results.jsx
@@ -1,7 +1,50 @@
 export default function Results() {
     return (
         <table id = 'result'>
-            
+            <thead>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
         </table>
     )
 }
