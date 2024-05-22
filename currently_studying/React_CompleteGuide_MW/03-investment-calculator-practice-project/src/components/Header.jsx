c6d2ddc71527c084bf35f7061d3f7d0cc047c882
Marissa Wilson <marfawilsoncode@gmail.com>
2024-05-22 15:13:10 -0500

    Created components folder with initial components & moved header element into Header.jsx

---
+++ currently_studying/React_CompleteGuide_MW/03-investment-calculator-practice-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header id="header">
+            <img src="investment-calculator-logo.png" alt="A bag of money surrounded by gold coins & graphs" />
+            <h1>React Investment Calculator</h1>
+        </header>
+    )
+}
