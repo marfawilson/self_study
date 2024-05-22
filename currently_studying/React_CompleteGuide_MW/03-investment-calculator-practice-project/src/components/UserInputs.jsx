1827d4f0e5b8a127bc62f19156882c5a3174e416
Marissa Wilson <marfawilsoncode@gmail.com>
2024-05-22 15:49:29 -0500

    Created initial user inputs

--- currently_studying/React_CompleteGuide_MW/03-investment-calculator-practice-project/src/components/UserInputs.jsx
+++ currently_studying/React_CompleteGuide_MW/03-investment-calculator-practice-project/src/components/UserInputs.jsx
@@ -1,3 +1,15 @@
 export default function UserInputs() {
-    
+    return (
+        <form className="input-group" id="user-input">
+            <p>
+                <label>Initial Investment<input type="number"></input></label>
+                <label>Annual Investment<input type="number"></input></label>
+            </p>
+
+            <p>
+                <label>Expected Return<input type="number"></input></label>
+                <label>Duration<input type="number"></input></label>
+            </p>
+        </form>
+    )
 }
