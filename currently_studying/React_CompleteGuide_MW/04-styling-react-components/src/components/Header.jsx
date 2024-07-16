8bddef07a45a45f5195d9011471dc0589f6f007f
Marissa Wilson <marfawilsoncode@gmail.com>
2024-07-16 10:55:30 -0500

    Added note to css import statement

--- currently_studying/React_CompleteGuide_MW/04-styling-react-components/src/components/Header.jsx
+++ currently_studying/React_CompleteGuide_MW/04-styling-react-components/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import './Header.css'; // even though we are importing a specific CSS file for this component, the styles are applied globally since they are injected into the head section of index.html
 
 export default function Header() {
   return (
