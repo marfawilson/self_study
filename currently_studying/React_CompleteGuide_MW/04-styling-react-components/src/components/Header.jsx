f4d507027cc0be445fb9c2e84e00d96fd9ebbfb1
Marissa Wilson <marfawilsoncode@gmail.com>
2024-07-17 14:53:50 -0500

    Updated p tag class via css module for proper scoping

--- currently_studying/React_CompleteGuide_MW/04-styling-react-components/src/components/Header.jsx
+++ currently_studying/React_CompleteGuide_MW/04-styling-react-components/src/components/Header.jsx
@@ -1,12 +1,14 @@
 import logo from '../assets/logo.png';
-import './Header.css'; // even though we are importing a specific CSS file for this component, the styles are applied globally since they are injected into the head section of index.html
+import classes from './Header.module.css'; 
+
+// even though we are importing a specific CSS file for this component, the styles are applied globally since they are injected into the head section of index.html
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }
