ec3f753268855b3b10ed7764cd80d50f3334eb0f
Marissa Wilson <marfawilsoncode@gmail.com>
2024-08-27 15:52:24 -0500

    Replaced styled component with Tailwind utility classes

--- currently_studying/React_CompleteGuide_MW/04-styling-react-components/src/components/AuthInputs.jsx
+++ currently_studying/React_CompleteGuide_MW/04-styling-react-components/src/components/AuthInputs.jsx
@@ -1,15 +1,7 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 
 import Button from './Button.jsx';
 import Input from './Input.jsx';
-
-const ControlDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 
 export default function AuthInputs() {
@@ -33,8 +25,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlDiv>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
           <Input
             label = "Email"
             type = "email"
@@ -50,10 +42,10 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-      </ControlDiv>
+      </div>
 
-      <div className = "actions">
-        <button type = "button" className = "text-button">
+      <div className = "flex justify-end gap-4">
+        <button type = "button" className = "text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick = {handleLogin}>Sign In</Button>
